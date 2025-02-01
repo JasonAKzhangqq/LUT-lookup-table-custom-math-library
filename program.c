c2c289b063e3951b89582232817f9697ccead6b4
JasonAK <2363090381@qq.com>
2025-02-01 09:38:26 +0800

    新增了h文件

--- program.c
+++ program.c
@@ -1,21 +1,12 @@
 #include <stdio.h>
 #include <math.h>
+#include "program.h"
 
-// 常用的值，以及相关配置
-#define M_PI       3.1415926535897932
-#define M_PI_M2    6.2831853071795862        //pi*2
-#define M_PI_D2    1.5707963267948965        //pi/2 
-#define M_180_D_PI  57.29577951308232        //180/pi
-#define M_PI_D180  0.017453292519943        //pi/180
-#define RADIAN_MIN -M_PI_D2
-#define RADIAN_MAX M_PI_D2
-#define RADIAN_STEP 0.001
-#define TABLE_SIZE ((int)( M_PI_D2 / RADIAN_STEP ) + 1 )//ֻ只记录0到pi/2的表
 
 // 各个表
 double sin_table[TABLE_SIZE];
 //double cos_table[TABLE_SIZE];
-//double tan_table[TABLE_SIZE];
+double tan_table[TABLE_SIZE];
 //double arcsin_table[TABLE_SIZE];
 //double arccos_table[TABLE_SIZE];
 double arctan_table[TABLE_SIZE];
@@ -25,6 +16,7 @@ void init_tables() {
     for (int i = 0; i < (TABLE_SIZE - 1); i++) {
         double radian =((i * RADIAN_STEP));
         sin_table[i] = sin(radian);
+        tan_table[i] = tan(radian);
         arctan_table[i] = atan(tan(radian));
     }
     sin_table[TABLE_SIZE - 1] = 1.00;
@@ -147,11 +139,11 @@ double tan_lookup(double radian)
 //    return arccos_table[index];
 //}
 //
-//// ʹ�ò��ұ����� arctan
-//double arctan_lookup(double value) {
-//    int index = find_closest_index(value, tan_table);
-//    return arctan_table[index];
-//}
+// ʹ�ò��ұ����� arctan
+double arctan_lookup(double value) {
+    int index = find_closest_index(value, tan_table);
+    return arctan_table[index];
+}
 //
 //// ʹ�ò��ұ����� atan2
 //double atan2_lookup(double y, double x) {
@@ -222,7 +214,8 @@ int main() {
     // 测试
     for (int i = 0; i < TABLE_SIZE; i++)
     {
-        printf("%.15f,\n", arctan_table[i]);
+        printf("%.15f,\n", sin_table[i]);
+        printf("%.15f,\n", tan_table[i]);
     }
     //printf("%.15f,\n", atan(0.815298612529843));
 
---
+++ program.h
@@ -0,0 +1,14 @@
+﻿//#ifndef _PROGRAM_H_		//用于嵌入式编程
+#define _PROGRAM_H_
+
+// 常用的值，以及相关配置
+#define M_PI       3.1415926535897932
+#define M_PI_M2    6.2831853071795862        //pi*2
+#define M_PI_D2    1.5707963267948965        //pi/2 
+#define M_180_D_PI  57.29577951308232        //180/pi
+#define M_PI_D180  0.017453292519943        //pi/180
+#define RADIAN_MIN -M_PI_D2
+#define RADIAN_MAX M_PI_D2
+#define RADIAN_STEP 0.001
+#define TABLE_SIZE ((int)( M_PI_D2 / RADIAN_STEP ) + 1 )//ֻ只记录0到pi/2的表
+
